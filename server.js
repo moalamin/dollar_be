274fcedc1c86925046a874e021e1a0dad64c7b03
Mo Amin <m.alamin92@gmail.com>
2017-11-23 22:32:38 -0500

    body parser json

--- server.js
+++ server.js
@@ -1,14 +1,33 @@
 let express = require('express');
 let app = express();
+let bodyParser = require('body-parser');
 let morgan = require('morgan');
-const PORT = (process.env.PORT || 3500); 
+const PORT = (process.env.PORT || 3500);
 
-app.use(morgan('combined'))
+//middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+app.use(morgan('combined'));
 
 app.get('/', (req, res) => {
-    res.json({"hello": "World"});
+    res.send('Use /api for making requests to Dellar API');
 });
+
+app.post('/api/charge', (req, res) => {
+    var token = request.body.stripeToken; // Using Express
+
+    // Charge the user's card:
+    stripe.charges.create({
+      amount: 100,
+      currency: "usd",
+      description: "Someone waster a dollar.",
+      source: token,
+    }, function(err, charge) {
+      // asynchronously called
+    });
+})
 
 app.listen(PORT, ()=> {
     console.log('App is now running on port: ' + PORT)
-})
+};
