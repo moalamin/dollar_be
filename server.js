56c8a9cac883c63a14dd63d6eeca9647b7e930e1
Mo Amin <m.alamin92@gmail.com>
2017-11-27 20:52:42 -0500

    listen for events on socket

--- server.js
+++ server.js
@@ -35,66 +35,78 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(morgan('combined'));
 
+//socket-io event
+io.on('connection', function(socket) {
+	socket.on('dollarCountRequest', function(socket) {
+		Charge.count(function(err, count) {
+			if (err) {
+				next(err);
+			} else {
+				io.emit('dollarCountTotal', {count});
+			}
+		});
+	});
+});
+
 app.get('/', (req, res) => {
-    res.send('Use /api for making requests to Dellar API');
+	res.send('Use /api for making requests to Dellar API');
 });
 
 app.post('/api/charge', (req, res, next) => {
-    var token = req.body.stripeToken; // Using Express
-    // Charge the user's card:
-    stripe.charges.create(
-        {
-            amount: 100,
-            currency: 'usd',
-            description: 'Someone wasted a dollar.',
-            source: token
-        },
-        function(err, charge) {
-            if (err) {
-                res.json(err);
-            } else {
-                let newCharge = new Charge({
-                    chargeId: charge.id,
-                    amount: charge.amount,
-                    source: charge.source,
-                    description: charge.description,
-                    paid: charge.paid
-                })
-                newCharge.save(function(err, savedCharge) {
-                    if (err) {
-                        next(err)
-                    } else {
-                        console.dir(savedCharge)
-                        res.json(charge);
-                    }
-                })
-            }
-        }
-    );
-});
-
-io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+	var token = req.body.stripeToken; // Using Express
+	// Charge the user's card:
+	stripe.charges.create(
+		{
+			amount: 100,
+			currency: 'usd',
+			description: 'Someone wasted a dollar.',
+			source: token
+		},
+		function(err, charge) {
+			if (err) {
+				res.json(err);
+			} else {
+				let newCharge = new Charge({
+					chargeId: charge.id,
+					amount: charge.amount,
+					source: charge.source,
+					description: charge.description,
+					paid: charge.paid
+				});
+				newCharge.save(function(err, savedCharge) {
+					if (err) {
+						next(err);
+					} else {
+						Charge.count(function(err, count) {
+							if (err) {
+								next(err);
+							} else {
+								io.emit('dollarCountTotal', {count});
+							}
+						});
+						res.json(charge);
+					}
+				});
+			}
+		}
+	);
 });
 
 app.get('/api/dollar_count', (req, res, next) => {
-     Charge.count(function(err, count){
-        if (err) {
-            next(err)
-        } else {
-            res.json({count: count});
-        }
-     });
+	Charge.count(function(err, count) {
+		if (err) {
+			next(err);
+		} else {
+			res.json({ count: count });
+		}
+	});
 });
 
 mongoose.connect(config.MONGO_DB_ENPOINT, function(err) {
-    if (err) {
-        throw err;
-    }
-    server.listen(PORT, () => {
-        console.log('App is now running on port: ' + PORT);
-    });
+	if (err) {
+		throw err;
+	}
+	server.listen(PORT, () => {
+		console.log('App is now running on port: ' + PORT);
+	});
 });
